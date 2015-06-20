1bc7dc5195b01d34c8e796af59d000e4ccef6d24
Faddey <yarik.pro.ph@gmail.com>
2015-06-20 21:58:57 +0300

    implemented one (most important) test for @raise_on_false

--- rest_utils/tests.py
+++ rest_utils/tests.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from mock import Mock
+from unittest.case import _AssertRaisesContext
 
 from django.http import Http404
 from django_nose import FastFixtureTestCase as TestCase
 
 from .permissions import DenyCreateOnPutPermission, NotAuthenticatedPermission
+from .decorators import raise_on_false
 
 
 class PermissionsTest(TestCase):
@@ -34,3 +36,45 @@ class PermissionsTest(TestCase):
 
         request.user.is_authenticated = Mock(return_value=False)
         self.assertTrue(permission.has_permission(request, view))
+
+
+class RaiseOnFalseTest(TestCase):
+
+    def assertNotRaises(self, callable_obj, *args, **kwargs):
+        try:
+            callable_obj(*args, **kwargs)
+        except:
+            self.fail("Function raises an exception unexpectedly")
+
+    def test_raising(self):
+        return_false = Mock(return_value=False)
+        return_true = Mock(return_value=True)
+
+        raising_decorator = raise_on_false(default_raises=True)
+        silent_decorator = raise_on_false(default_raises=False)
+
+        raising = raising_decorator(return_false)
+        # This function will always raise an exception
+        # unless we'll silent it explicitly
+        self.assertRaises(BaseException, raising)
+        self.assertRaises(BaseException, raising, raise_exception=True)
+        self.assertNotRaises(raising, raise_exception=False)
+
+        silent = silent_decorator(return_false)
+        # This function will raise an exception
+        # only if we explicitly specify it
+        self.assertNotRaises(silent)
+        self.assertNotRaises(silent, raise_exception=False)
+        self.assertRaises(BaseException, silent, raise_exception=True)
+
+        never_raises = raising_decorator(return_true)
+        # This function never raise exception because it returns True
+        self.assertNotRaises(never_raises)
+        self.assertNotRaises(never_raises, raise_exception=True)
+        self.assertNotRaises(never_raises, raise_exception=False)
+
+        never_raises = silent_decorator(return_true)
+        # This function never raise exception because it returns True
+        self.assertNotRaises(never_raises)
+        self.assertNotRaises(never_raises, raise_exception=True)
+        self.assertNotRaises(never_raises, raise_exception=False)
