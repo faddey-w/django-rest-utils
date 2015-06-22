ed772f9c09fc8d4f3f898c1b295c21ebc908e2e7
Faddey <yarik.pro.ph@gmail.com>
2015-06-22 14:08:03 +0300

    implemented @cached_property, partially tested

--- rest_utils/decorators/cached_property.py
+++ rest_utils/decorators/cached_property.py
@@ -1,64 +1,99 @@
 __author__ = 'faddey'
 
-_empty = object()
+from functools import wraps
+from django.utils.decorators import available_attrs
 
 
-class cached_property(object):
+class read_only_cached_property(object):
 
-    _cached_values = {}
+    def __init__(self, fget=None, doc=None, name=None):
+        self.getter(fget)
+        self.name = getattr(fget, '__name__', name)
+        assert isinstance(self.name, str), 'Cannot resolve property name'
+        self.__doc__ = doc or fget.__doc__
 
     def getter(self, function):
-        if function:
-            def fget(obj, type=None):
-                cached = self._cached_values.get(id(obj), _empty)
-                if cached is _empty:
-                    cached = function(obj)
-                    self._cached_values[id(obj)] = cached
-                return cached
-            self.fget = fget
-        else:
-            self.fget = None
+        self.fget = function
         return self
 
     def setter(self, function):
-        if function:
-            def fset(obj, value):
-                self._cached_values[id(obj)] = _empty
-                return function(obj, value)
-            self.fset = fset
-        else:
-            self.fset = None
-        return self
+        return read_write_cached_property(
+            fget=self.fget,
+            fset=function,
+            doc=self.__doc__
+        )
 
     def deleter(self, function):
-        def fdel(obj):
-            self._cached_values[id(obj)] = _empty
-            if function:
-                return function(obj)
-        self.fdel = fdel
-        return self
+        return read_write_cached_property(
+            fget=self.fget,
+            fdel=function,
+            doc=self.__doc__
+        )
+
+    def invalidator(self, function):
+        def wrapper(obj, *args, **kwargs):
+            ret = function(obj, *args, **kwargs)
+            self._invalidate(obj)
+            return ret
+        return wraps(function, assigned=available_attrs(function))(wrapper)
+
+    def is_cached(self, obj):
+        return self.name in obj.__dict__
+
+    def _invalidate(self, obj):
+        obj.__dict__.pop(self.name, None)
 
     def __get__(self, obj, objtype=None):
         if obj is None:
             return self
         if self.fget is None:
             raise AttributeError("unreadable attribute")
-        return self.fget(obj)
+        cached = self.fget(obj)
+        obj.__dict__[self.name] = cached
+        return cached
+
+
+_empty = object()
+
+
+class read_write_cached_property(read_only_cached_property):
+
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None, name=None):
+        name = getattr(fset or fdel, '__name__', name)
+        super(read_write_cached_property, self).__init__(fget, doc, name)
+        self.setter(fset)
+        self.deleter(fdel)
+
+    def setter(self, function):
+        self.fset = function
+        return self
+
+    def deleter(self, function):
+        self.fdel = function
+        return self
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        cached = obj.__dict__.get(self.name, _empty)
+        if cached is _empty:
+            cached = super(read_write_cached_property, self).__get__(obj, objtype)
+        return cached
 
     def __set__(self, obj, value):
         if self.fset is None:
             raise AttributeError("can't set attribute")
         self.fset(obj, value)
+        self._invalidate(obj)
 
     def __delete__(self, obj):
-        if self.fdel is None:
-            raise AttributeError("can't delete attribute")
-        self.fdel(obj)
+        if self.fdel:
+            self.fdel(obj)
+        self._invalidate(obj)
 
-    def __init__(self, fget=None, fset=None, fdel=None, doc=None):
-        self.getter(fget)
-        self.setter(fset)
-        self.deleter(fdel)
-        if doc is None and fget is not None:
-            doc = fget.__doc__
-        self.__doc__ = doc
+
+def cached_property(fget=None, fset=None, fdel=None, doc=None, name=None):
+    if fset or fdel:
+        return read_write_cached_property(fget, fset, fdel, doc, name)
+    else:
+        return read_only_cached_property(fget, doc, name)
--- rest_utils/tests.py
+++ rest_utils/tests.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 from mock import Mock
 from unittest import skip
+import traceback
 
 from django.http import Http404
 # from django_nose import FastFixtureTestCase as TestCase
 from django.test import TestCase
 
 from .permissions import DenyCreateOnPutPermission, NotAuthenticatedPermission
-from .decorators import raise_on_false
+from .decorators import raise_on_false, cached_property
+from .decorators.cached_property import read_write_cached_property, read_only_cached_property
+
+
+class AssertNotRaisesMixin(object):
+
+    def assertNotRaises(self, callable_obj, *args, **kwargs):
+        try:
+            callable_obj(*args, **kwargs)
+        except:
+            msg = ("Function raises an exception unexpectedly\n"
+                   "Original exception was:\n\n" + traceback.format_exc())
+            self.fail(msg)
 
 
 @skip
@@ -40,13 +53,7 @@ class PermissionsTest(TestCase):
         self.assertTrue(permission.has_permission(request, view))
 
 
-class RaiseOnFalseTest(TestCase):
-
-    def assertNotRaises(self, callable_obj, *args, **kwargs):
-        try:
-            callable_obj(*args, **kwargs)
-        except:
-            self.fail("Function raises an exception unexpectedly")
+class RaiseOnFalseTest(AssertNotRaisesMixin, TestCase):
 
     def test_raising(self):
         return_false = Mock(return_value=False)
@@ -95,3 +102,117 @@ class RaiseOnFalseTest(TestCase):
             BaseException, reverse(arg),
             f, arg, raise_exception=True
         )
+
+
+class CachedPropertyTestCase(AssertNotRaisesMixin, TestCase):
+
+    def test_magic(self):
+
+        def function():
+            f = lambda *args, **kwargs: None
+            f.__name__ = 'name'
+            return f
+
+        # cannot resolve property name
+        self.assertRaises(AssertionError, cached_property)
+
+        empty = cached_property(name='empty')
+        self.assertIsInstance(empty, read_only_cached_property)
+        self.assertNotIsInstance(empty, read_write_cached_property)
+
+        read_only = cached_property(function())
+        self.assertIsInstance(read_only, read_only_cached_property)
+        self.assertNotIsInstance(read_only, read_write_cached_property)
+
+        read_write = read_only.setter(function())
+        self.assertIsInstance(read_write, read_write_cached_property)
+
+        read_write = read_only.deleter(function())
+        self.assertIsInstance(read_write, read_write_cached_property)
+
+        props = [
+            cached_property(fset=function()),
+            cached_property(fdel=function()),
+            cached_property(fset=function(), fdel=function()),
+            cached_property(fget=function(), fset=function()),
+            cached_property(fget=function(), fdel=function()),
+            cached_property(fget=function(), fset=function(), fdel=function()),
+        ]
+        for read_write in props:
+            self.assertIsInstance(read_write, read_write_cached_property)
+
+    def test_caching(self):
+
+        class Class(object):
+            data = None
+
+            @cached_property
+            def prop(self):
+                return self.data
+
+        obj = Class()
+
+        # setting `data` does not creating cached value
+        obj.data = 1
+        self.assertFalse(Class.prop.is_cached(obj))
+
+        # accessing `prop` creates cached value
+        self.assertEqual(obj.prop, 1)
+        self.assertTrue(Class.prop.is_cached(obj))
+
+        # modifying `data` does not changes cached value
+        obj.data = 2
+        self.assertTrue(Class.prop.is_cached(obj))
+        self.assertEqual(obj.prop, 1)
+
+        # deleting `data` does not changes cached value
+        del obj.data
+        self.assertTrue(Class.prop.is_cached(obj))
+        self.assertEqual(obj.prop, 1)
+
+    def test_invalidating(self):
+
+        class Class(object):
+            data = None
+
+            @cached_property
+            def prop(self):
+                return self.data
+
+            @prop.invalidator
+            def modify(self, value):
+                self.data = value
+
+            @prop.invalidator
+            def just_invalidate(self):
+                pass
+
+        obj = Class()
+
+        # invalidation does not raises when cached value not set
+        self.assertNotRaises(obj.modify, 1)
+        self.assertEqual(obj.data, 1)
+        self.assertFalse(Class.prop.is_cached(obj))
+
+        # calling invalidation methods invalidates cache
+        obj.prop
+        obj.just_invalidate()
+        self.assertFalse(Class.prop.is_cached(obj))
+        obj.prop
+        obj.modify(2)
+        self.assertFalse(Class.prop.is_cached(obj))
+
+    def test_setter(self):
+        class Class(object):
+            data = None
+
+            @cached_property
+            def prop(self):
+                return self.data
+
+            @prop.setter
+            def prop(self, value):
+                self.data = value
+
+        obj = Class()
+
