8e3c4d6ab1de9dadf03e2ee6809f419ff3dbfad4
Faddey <yarik.pro.ph@gmail.com>
2015-06-23 15:23:12 +0100

    beautified ModifiableRouter

--- rest_utils/routers.py
+++ rest_utils/routers.py
@@ -1,55 +1,25 @@
 __author__ = 'faddey'
 
+from rest_framework.routers import DefaultRouter
+from rest_framework.decorators import detail_route, list_route
 
-class ModifiedDefaultRouter(DefaultRouter):
 
-    """
-    little hack: override default routes list for capability to add
-    more routes and more http methods for it. e.g. see list route
-    i've added method PATCH for it
-    """
+class ModifiableRouterMixin(object):
 
-    _routes = [
-        # List route.
-        Route(
-            url=r'^{prefix}{trailing_slash}$',
-            mapping={
-                'get': 'list',
-                'post': 'create',
-                'patch': 'global_update',
-            },
-            name='{basename}-list',
-            initkwargs={'suffix': 'List'}
-        ),
-        # Dynamically generated list routes.
-        # Generated using @list_route decorator
-        # on methods of the viewset.
-        DynamicListRoute(
-            url=r'^{prefix}/{methodname}{trailing_slash}$',
-            name='{basename}-{methodnamehyphen}',
-            initkwargs={}
-        ),
-        # Detail route.
-        Route(
-            url=r'^{prefix}/{lookup}{trailing_slash}$',
-            mapping={
-                'get': 'retrieve',
-                'put': 'update',
-                'patch': 'partial_update',
-                'delete': 'destroy'
-            },
-            name='{basename}-detail',
-            initkwargs={'suffix': 'Instance'}
-        ),
-        # Dynamically generated detail routes.
-        # Generated using @detail_route decorator on methods of the viewset.
-        DynamicDetailRoute(
-            url=r'^{prefix}/{lookup}/{methodname}{trailing_slash}$',
-            name='{basename}-{methodnamehyphen}',
-            initkwargs={}
-        ),
-    ]
+    _routes = list(DefaultRouter.routes)
 
     @property
     def routes(self):
         return self._routes
+
+    @property
+    def list_route(self):
+        return self.routes[0]
+
+    @property
+    def detail_route(self):
+        return self.routes[2]
+
+
+class ModifiableRouter(ModifiableRouterMixin, DefaultRouter):
+    pass
