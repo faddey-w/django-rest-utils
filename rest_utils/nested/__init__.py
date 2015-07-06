2f995ac1f00069be404af1bf57938eaadd72d66d
Faddey <yarik.pro.ph@gmail.com>
2015-07-06 18:23:10 +0300

    removed rudimental files

--- rest_utils/nested/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'faddey'
-
-from rest_framework.viewsets import GenericViewSet
-
-
-class NestedViewSet(GenericViewSet):
-
-    def dispatch(self, request, *args, **kwargs):
-        pass
